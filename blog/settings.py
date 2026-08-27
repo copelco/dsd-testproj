74c72ba2a3a7c0a45b6003c287951900864678d5
Colin Copeland <copelco@caktusgroup.com>
2026-08-27 16:36:00 +0000

    Configured project for deployment.

--- blog/settings.py
+++ blog/settings.py
@@ -243,3 +243,31 @@ if os.getenv("ON_PYTHONANYWHERE"):
     }
 
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
+# PythonAnywhere settings.
+import os  # noqa: E402
+
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except ImportError:
+    pass
+
+if os.getenv("ON_PYTHONANYWHERE"):
+    import dj_database_url
+
+    DEBUG = os.getenv("DEBUG") == "TRUE"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    try:
+        ALLOWED_HOSTS.append("copelcobeginner.pythonanywhere.com")
+    except NameError:
+        ALLOWED_HOSTS = ["copelcobeginner.pythonanywhere.com"]
+
+    DATABASES = {
+        "default": dj_database_url.config(),
+    }
+
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
